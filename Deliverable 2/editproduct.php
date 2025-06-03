a3df8c8f054092b9207d1fa26ac7289bd8cbe4e5
whileNotEOThori <140858944+whileNotEOThori@users.noreply.github.com>
2025-06-03 23:43:33 +0200

    Refactor: Implement editProduct method in seller class and streamline product editing process in editproduct.php

--- Deliverable 2/editproduct.php
+++ Deliverable 2/editproduct.php
@@ -1,58 +1,10 @@
 <?php
-session_start();
 require_once('sellerfunctions.php');
+require_once('seller.php');
+session_start();
 
 if (isset($_POST['editProduct'])) {
-    //retrieve/extract the edited product information entered in the form
-    $productID = $_POST['productID+Name'];
-    $productName = $_POST['productName'];
-    $productDescription = $_POST['productDescription'];
-    $productCategory = $_POST['productCategory'];
-    $productMass = $_POST['productMass'];
-    $productPrice = $_POST['productPrice'];
-    $productQuantity = $_POST['productQuantity'];
-    $file = $_FILES['productImage'];
+    $_SESSION['seller']->editProduct();
 
-    if (empty($productID)) die("Error: Product ID is empty or invalid." . $productID);
-
-    if ($productID == "") {
-        echo "<script> alert('You have not selected a product to edit')</script>";
-        exit;
-    }
-
-    if ($productName == "" && $productDescription == "" && $productCategory == "" && $productMass == "" && $productPrice == "" && $productQuantity == "") {
-        echo "<script> alert('You have made an edits to the selected product')</script>";
-        exit;
-    }
-
-    // get product data to create an object
-    $productRow = getProductRow($productID);
-    if ($productRow == null || $productRow == false) {
-        echo "<script> alert('The product you have selected does not exist so we cannot make any edits to it')</script>";
-        exit;
-    }
-    $product = new product($productRow);
-
-    //assign the product with the new/edited values
-    if ($productName != "") $product->name = $productName;
-
-    if ($productDescription != "") $product->description = $productDescription;
-
-    if ($productCategory != "") $product->cID = $productCategory;
-
-    if ($productMass != "") $product->mass = $productMass;
-
-    if ($productPrice != "") $product->price = $productPrice;
-
-    if ($productQuantity != "") $product->quantity = $productQuantity;
-
-    //do the same for image
-
-    if (!editProductData($product)) {
-        echo "<script> alert('There was an error editing the product you have selected')</script>";
-        exit;
-    }
-
-    echo "<script> alert('you have successfully edited the product')</script>";
     header('location: editproductpage.php');
 }
--- Deliverable 2/seller.php
+++ Deliverable 2/seller.php
@@ -1,5 +1,6 @@
 <?php
 require_once("user.php");
+require_once('sellerfunctions.php');
 
 class seller extends user
 {
@@ -246,7 +247,7 @@ class seller extends user
     public function addProduct()
     {
         global $conn;
-        
+
         //retrieve/extract the information entered in the form
         $productName = $_POST['productName'];
         $productDescription = $_POST['productDescription'];
@@ -305,5 +306,61 @@ class seller extends user
             die("Add product query execution failed: " . $addProductQuery->error);
 
         $addProductQuery->close();
+    }
+
+    public function editProduct()
+    {
+        global $conn;
+        //retrieve/extract the edited product information entered in the form
+        $productID = $_POST['productID+Name'];
+        $productName = $_POST['productName'];
+        $productDescription = $_POST['productDescription'];
+        $productCategory = $_POST['productCategory'];
+        $productMass = $_POST['productMass'];
+        $productPrice = $_POST['productPrice'];
+        $productQuantity = $_POST['productQuantity'];
+        $file = $_FILES['productImage'];
+
+        if (empty($productID)) die("Error: Product ID is empty or invalid." . $productID);
+
+        if ($productID == "") {
+            echo "<script> alert('You have not selected a product to edit')</script>";
+            exit;
+        }
+
+        if ($productName == "" && $productDescription == "" && $productCategory == "" && $productMass == "" && $productPrice == "" && $productQuantity == "") {
+            echo "<script> alert('You have made an edits to the selected product')</script>";
+            exit;
+        }
+
+        // get product data to create an object
+        $productRow = getProductRow($productID);
+        if ($productRow == null || $productRow == false) {
+            echo "<script> alert('The product you have selected does not exist so we cannot make any edits to it')</script>";
+            exit;
+        }
+        $product = new product($productRow);
+
+        //assign the product with the new/edited values
+        if ($productName != "") $product->name = $productName;
+
+        if ($productDescription != "") $product->description = $productDescription;
+
+        if ($productCategory != "") $product->cID = $productCategory;
+
+        if ($productMass != "") $product->mass = $productMass;
+
+        if ($productPrice != "") $product->price = $productPrice;
+
+        if ($productQuantity != "") $product->quantity = $productQuantity;
+
+        //do the same for image
+
+        if (!editProductData($product)) {
+            echo "<script> alert('There was an error editing the product you have selected')</script>";
+            exit;
+        }
+
+        echo "<script> alert('you have successfully edited the product')</script>";
     }
 }
